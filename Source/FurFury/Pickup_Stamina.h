b87996439d2bd2187f9ba002217950b3ec5bc753
yearlyfruit <60387208+yearlyfruit@users.noreply.github.com>
2020-05-23 15:15:44 +0200

    brage work
    ops wrong , this is the real one

---
+++ Source/FurFury/Pickup_Stamina.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/SphereComponent.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup_Stamina.generated.h"
+
+UCLASS()
+class FURFURY_API APickup_Stamina : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickup_Stamina();
+	
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	UStaticMeshComponent* ManaMesh;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
