3f1b5735d988972a1354925d471d4667b11e3c1f
master <mohamedkwaik@yahoo.com>
2020-05-28 15:15:03 +0200

    Fixed spawner issues

--- Source/FurFury/EnemySpawner.cpp
+++ Source/FurFury/EnemySpawner.cpp
@@ -11,7 +11,7 @@
 AEnemySpawner::AEnemySpawner()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 
 	MainRootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("DefRootComp"));
 	SetRootComponent(MainRootComponent);
@@ -26,10 +26,10 @@ AEnemySpawner::AEnemySpawner()
 void AEnemySpawner::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	if(bInfiniteSpawns){
+	if(bInfiniteSpawns == true){
 		UE_LOG(LogTemp, Warning, TEXT("Using Unlimited Spawns"))
 		ReadyToSpawn = true;
-	}else if(!bInfiniteSpawns && AmountOfActors > 0){
+	}else if(bInfiniteSpawns == false && AmountOfActors > 0){
 		UE_LOG(LogTemp, Warning, TEXT("Using Limited Spawns: Amount Of Actors Left To Spawn %d"), AmountOfActors);
 		ReadyToSpawn = true;
 	}else{
@@ -55,28 +55,32 @@ void AEnemySpawner::BeginPlay()
 			UE_LOG(LogTemp, Warning, TEXT("Using Static Preset Delay"))
 				Final = LocalPreSetDelayInSeconds;
 		}
-	GetWorldTimerManager().SetTimer(SpawnTimer, this, &AEnemySpawner::spawn, Final, ReadyToSpawn);
+	GetWorldTimerManager().SetTimer(SpawnTimer, this, &AEnemySpawner::spawn, Final, true);
 }
 
 
 //changed to this code due to bug.
 void AEnemySpawner::spawn() {
-	UWorld* world = GetWorld();
-	FRotator rotatingside = UKismetMathLibrary::RandomRotator();
-	if(!SpawnLocationRandomWithinBoxLimits)
+	if(ReadyToSpawn)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Using Exact Location"))
-		FVector spawnlocation = RootComponent->GetComponentLocation();	
-	}else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Using Random Location Within An Area"))
-		FVector spawnlocation = UKismetMathLibrary::RandomPointInBoundingBox(RootComponent->GetComponentLocation(), spawnbox->GetScaledBoxExtent());
-	}
+		UWorld* world = GetWorld();
+		FRotator rotatingside = RootComponent->GetRelativeRotation();
+		if(!SpawnLocationRandomWithinBoxLimits)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Using Exact Location"))
+			spawnlocation = RootComponent->GetComponentLocation();	
+		}else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Using Random Location Within An Area"))
+			spawnlocation = UKismetMathLibrary::RandomPointInBoundingBox(RootComponent->GetComponentLocation(), spawnbox->GetScaledBoxExtent());
+		}
 
-	
-	if (world) {
-			UE_LOG(LogTemp, Warning, TEXT("Spawning"))
-			world->SpawnActor<ACharacter>(ActorToSpawn, spawnlocation, rotatingside);
+		
+		if (world) {
+				UE_LOG(LogTemp, Warning, TEXT("Spawning"))
+				world->SpawnActor<ACharacter>(ActorToSpawn, spawnlocation, rotatingside);
+				AmountOfActors--;
+		}	
 	}
 }
 
